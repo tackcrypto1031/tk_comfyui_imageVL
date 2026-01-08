e77fac18924533d7a77ab2edb396a7e733ce695e
AUERAD\Auer0993 <tack_lin@auer.com.tw>
2026-01-08 11:59:14 +0800

    新增單圖反推

--- __init__.py
+++ __init__.py
@@ -1,17 +1,3 @@
-from .nodes import TK_BatchImageLoader, TK_QwenVL_Interrogator, TK_TextSaver, TK_JoyCaption_Interrogator
-
-NODE_CLASS_MAPPINGS = {
-    "TK_BatchImageLoader": TK_BatchImageLoader,
-    "TK_QwenVL_Interrogator": TK_QwenVL_Interrogator,
-    "TK_TextSaver": TK_TextSaver,
-    "TK_JoyCaption_Interrogator": TK_JoyCaption_Interrogator
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "TK_BatchImageLoader": "TK Batch Image Loader",
-    "TK_QwenVL_Interrogator": "TK QwenVL Interrogator",
-    "TK_TextSaver": "TK Text Saver",
-    "TK_JoyCaption_Interrogator": "TK JoyCaption Interrogator"
-}
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
